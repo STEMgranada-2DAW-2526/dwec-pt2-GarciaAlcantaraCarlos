132d8a4a7955d3f9fafb04fd6a19351bc5a3da1e
GarciaAlcantaraCarlos <garciaalcantaracarlos001.alu@stemgranada.com>
2025-11-25 10:58:44 +0100

    timed commit

--- src/GameProvider.jsx
+++ src/GameProvider.jsx
@@ -1,14 +1,24 @@
+import { createContext, useState } from "react"
+
 const initialState = {
   damageDealt: 0,
   waveGoal: 100,
   caramels: 20,
   gamaePerShot: 1,
   authoShotsPerSecond: 1,
+  
   upgrades: []
 }
 
+export const GameContext = createContext();
+
 export default function GameProvider() {
 
-  const Game
-  
+  const [test, setTest] = useState('test')
+
+  return (
+    <GameContext value={ test }>
+      {children}
+    </GameContext>
+  )
 }
