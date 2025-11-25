64594fcbd07dbab06f9279a0a48dd6d9e4860650
GarciaAlcantaraCarlos <garciaalcantaracarlos001.alu@stemgranada.com>
2025-11-25 10:52:37 +0100

    timed commit

---
+++ src/GameProvider.jsx
@@ -0,0 +1,14 @@
+const initialState = {
+  damageDealt: 0,
+  waveGoal: 100,
+  caramels: 20,
+  gamaePerShot: 1,
+  authoShotsPerSecond: 1,
+  upgrades: []
+}
+
+export default function GameProvider() {
+
+  const Game
+  
+}
